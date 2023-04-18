6c49846284cec52b65b2a1b434d65dffc9e21fe2
0852FeiFeiLin <2778368047@qq.com>
2023-04-19 00:04:56 +0800

    feat: 添加alipay拓展方法

--- internal/logic/internal/aliyun/aliyun.go
+++ internal/logic/internal/aliyun/aliyun.go
@@ -3,12 +3,17 @@ package aliyun
 import (
 	"context"
 	"crypto"
+	"crypto/md5"
 	"crypto/rand"
 	"crypto/rsa"
 	"crypto/sha1"
 	"crypto/sha256"
+	"crypto/x509"
 	"encoding/base64"
+	"encoding/hex"
 	"encoding/json"
+	"encoding/pem"
+	"errors"
 	"fmt"
 	"github.com/SupenBysz/gf-admin-community/sys_service"
 	"github.com/gogf/gf/v2/encoding/gxml"
@@ -23,6 +28,9 @@ import (
 	"github.com/kysion/gopay/pkg/xpem"
 	"github.com/kysion/gopay/pkg/xrsa"
 	"hash"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"time"
 )
 
@@ -118,24 +126,6 @@ func NewClient(ctx context.Context, appId string) (client *AliPay, err error) {
 	return client, nil
 }
 
-// GetSignData 获取数据签名
-//func GetSignData(bs []byte, alipayCertSN string) (signData string, err error) {
-//	var (
-//		str        = string(bs)
-//		indexStart = strings.Index(str, `_response":`)
-//		indexEnd   int
-//	)
-//	indexStart = indexStart + 11
-//	bsLen := len(str)
-//	// 公钥证书模式
-//	indexEnd = strings.Index(str, `,"alipay_cert_sn":`)
-//	if indexEnd > indexStart && bsLen > indexStart {
-//		signData = str[indexStart:indexEnd]
-//		return
-//	}
-//	return gopay.NULL, fmt.Errorf("[%w], value: %s", gopay.GetSignDataErr, str)
-//}
-
 // 获取支付宝参数签名
 // bm：签名参数
 // signType：签名类型，alipay.RSA 或 alipay.RSA2
@@ -178,6 +168,9 @@ func (s *AliPay) GetRsaSign(bm gopay.BodyMap, signType string, privateKey string
 	if _, err = h.Write([]byte(signParams)); err != nil {
 		return
 	}
+
+	fmt.Println(h.Sum(nil))
+
 	if encryptedBytes, err = rsa.SignPKCS1v15(rand.Reader, priKey, hashs, h.Sum(nil)); err != nil {
 		return util.NULL, fmt.Errorf("[%w]: %+v", gopay.SignatureErr, err)
 	}
@@ -213,6 +206,7 @@ func (s *AliPay) PostAliPayAPISelfV2(ctx context.Context, bm gopay.BodyMap, meth
 // 向支付宝发送自定义请求
 func (s *AliPay) doAliPaySelf(ctx context.Context, bm gopay.BodyMap, method string) (bs []byte, err error) {
 	var (
+		//url, sign string
 		url, sign string
 	)
 	bm.Set("method", method)
@@ -231,15 +225,17 @@ func (s *AliPay) doAliPaySelf(ctx context.Context, bm gopay.BodyMap, method stri
 		xlog.Debugf("Alipay_Request: %s", bm.JsonBody())
 	}
 
-	httpClient := xhttp.NewClient()
-	//httpClient.SetBodySize()
-
 	if s.IsProd {
 		url = "https://openapi.alipay.com/gateway.do?charset=utf-8"
 	} else {
 		url = "https://openapi.alipaydev.com/gateway.do?charset=utf-8"
 	}
-	res, bs, err := httpClient.Type(xhttp.TypeForm).Post(url).SendString(bm.EncodeURLParams()).EndBytes(ctx)
+
+	// 错误返回：返回页面
+	httpClient := xhttp.NewClient()
+	//httpClient.SetBodySize()
+
+	res, bs, err := httpClient.Type(xhttp.TypeJSON).Post(url).SendBodyMap(bm).EndBytes(ctx) // TypeForm  .EncodeURLParams()
 	if err != nil {
 		return nil, err
 	}
@@ -250,6 +246,39 @@ func (s *AliPay) doAliPaySelf(ctx context.Context, bm gopay.BodyMap, method stri
 		return nil, fmt.Errorf("HTTP Request Error, StatusCode = %d", res.StatusCode)
 	}
 	return bs, nil
+
+	//// 错误返回：返回验签失败
+	//params := bm.EncodeURLParams()
+
+	//content := g.Client().PostContent(ctx, url, bm)
+	//fmt.Println("自定义请求：", content)
+	//return nil, nil
+}
+func DiyRequest(bm gopay.BodyMap) []byte {
+	// 1.创建一个客户端
+	client := http.Client{}
+	// 2.创建请求对象
+	params := bm.EncodeURLParams()
+	request, err := http.NewRequest("POST", params, nil)
+	if err != nil {
+		return nil
+	}
+
+	// 3.发起请求
+	response, err := client.Do(request)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+	if response.StatusCode != 200 {
+		return nil
+	}
+
+	// 4.读取响应数据
+	resByte, err := ioutil.ReadAll(response.Body)
+	fmt.Println(resByte)
+
+	return resByte
 }
 
 // 公共参数检查
@@ -310,4 +339,73 @@ func (s *AliPay) getRsaSign(bm gopay.BodyMap, signType string, privateKey *rsa.P
 	}
 	sign = base64.StdEncoding.EncodeToString(encryptedBytes)
 	return
+}
+
+// 需注意的是，公钥签名模式和公钥证书签名模式的不同之处
+// 验签文档：https://opendocs.alipay.com/open/200/106120
+func (s *AliPay) GetSignData(bs []byte) (signData string, err error) {
+	var (
+		str        = string(bs)
+		indexStart = strings.Index(str, `_response":`)
+		indexEnd   int
+	)
+	indexStart = indexStart + 11
+	bsLen := len(str)
+	alipayCertSN, _ := alipay.GetCertSN(s.AliPayPublicCertSN)
+
+	if alipayCertSN != "" {
+		// 公钥证书模式
+		if alipayCertSN != s.AliPayPublicCertSN {
+			return gopay.NULL, fmt.Errorf("[%w], 当前使用的支付宝公钥证书SN[%s]与网关响应报文中的SN[%s]不匹配", gopay.CertNotMatchErr, s.AliPayPublicCertSN, alipayCertSN)
+		}
+		indexEnd = strings.Index(str, `,"alipay_cert_sn":`)
+		if indexEnd > indexStart && bsLen > indexStart {
+			signData = str[indexStart:indexEnd]
+			return
+		}
+		return gopay.NULL, fmt.Errorf("[%w], value: %s", gopay.GetSignDataErr, str)
+	}
+	// 普通公钥模式
+	indexEnd = strings.Index(str, `,"sign":`)
+	if indexEnd > indexStart && bsLen > indexStart {
+		signData = str[indexStart:indexEnd]
+		return
+	}
+	return gopay.NULL, fmt.Errorf("[%w], value: %s", gopay.GetSignDataErr, str)
+}
+
+// GetCertSN 获取证书序列号SN
+// certPathOrData x509证书文件路径(appCertPublicKey.crt、alipayCertPublicKey_RSA2.crt) 或证书 buffer
+// 返回 sn：证书序列号(app_cert_sn、alipay_cert_sn)
+// 返回 err：error 信息
+func GetCertSN(certPathOrData interface{}) (sn string, err error) {
+	var certData []byte
+	switch pathOrData := certPathOrData.(type) {
+	case string:
+		certData, err = ioutil.ReadFile(pathOrData)
+		if err != nil {
+			return util.NULL, err
+		}
+	case []byte:
+		certData = pathOrData
+	default:
+		return util.NULL, errors.New("certPathOrData 证书类型断言错误")
+	}
+
+	if block, _ := pem.Decode(certData); block != nil {
+		cert, err := x509.ParseCertificate(block.Bytes)
+		if err != nil {
+			return util.NULL, err
+		}
+		name := cert.Issuer.String()
+		serialNumber := cert.SerialNumber.String()
+		h := md5.New()
+		h.Write([]byte(name))
+		h.Write([]byte(serialNumber))
+		sn = hex.EncodeToString(h.Sum(nil))
+	}
+	if sn == util.NULL {
+		return util.NULL, errors.New("failed to get sn,please check your cert")
+	}
+	return sn, nil
 }
