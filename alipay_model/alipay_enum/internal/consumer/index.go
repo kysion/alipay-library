5fde6436a111e0fe1aff9123c402296131ec522d
Arise <2778368047@qq.com>
2024-01-27 20:32:21 +0800

    feat: 添加用户授权状态枚举

--- alipay_model/alipay_enum/internal/consumer/index.go
+++ alipay_model/alipay_enum/internal/consumer/index.go
@@ -4,10 +4,12 @@ type consumer struct {
 	SexEnum    sex
 	ActionEnum action
 	Category   category
+	AuthState  authState
 }
 
 var Consumer = consumer{
 	SexEnum:    SexType,
 	ActionEnum: ActionType,
 	Category:   CategoryType,
+	AuthState:  AuthState,
 }
